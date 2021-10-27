579972de77a23b903e04fc6f9a67e0d3a427c653
Ivan-Loboda <Ivan.Loboda.1996@gmail.com>
2021-10-27 18:39:47 +0300

    1 task?

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import ProfileList from './ProfileList';
+// import statisticalData from './statisticalData.json';
+// import friends from './friends.json';
+// import Statistics from './Statistic/Statistics';
+// import FriendsList from './Friends/FriendsList';
+
+function App() {
+  return (
+    <div className="App">
+      <ProfileList />
+      {/* <Statistics title={'Upload stats'} stats={statisticalData} /> */}
+      {/* <FriendsList friends={friends} /> */}
+    </div>
+  );
+}
+
+export default App;
