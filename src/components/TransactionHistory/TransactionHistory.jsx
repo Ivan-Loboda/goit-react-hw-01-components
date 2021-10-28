c08a7530a9e7811f3077b116eb6efd62ba777074
Ivan-Loboda <Ivan.Loboda.1996@gmail.com>
2021-10-28 21:29:30 +0300

    в догонку к исправлениям

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -27,9 +27,6 @@ TransactionHistory.protoTypes = {
     items: PropTypes.arrayOf(
         PropTypes.shape({
             id: PropTypes.string.isRequired,
-            type: PropTypes.string.isRequired,
-            amount: PropTypes.number.isRequired,
-            currency: PropTypes.string.isRequired,
         }),
     ).isRequired,
 };
